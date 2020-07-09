c823139c89ef2dc7086a633d449bb8dbd0f24f31
eaw10-20 <60686880+eaw10-20@users.noreply.github.com>
2020-07-09 09:41:29 -0700

    Update Audio-Converter.py
    Added question for replacement of existing file, and updated formatting to include second button and textbox.

--- Audio-Converter.py
+++ Audio-Converter.py
@@ -1,36 +1,53 @@
 from pydub import AudioSegment
 from tkinter import *
-from tkinter import filedialog # why do I need this?!?!?!?!?!
-import os
+from tkinter import filedialog, messagebox # why do I need this?!?!?!?!?!
+import os, os.path
 
 root = Tk()
+root.title("Audio Converter")
 
 def mp3_to_ogg():
+	cont = True
 	# file to be converted
 	fileDir = filedialog.askopenfilename(initialdir="/", title="Select File",
 		filetypes=(("mp3", "*.mp3"),))
 	# get the full file name from path (e.g. audio.mp3)
 	noExten = os.path.splitext(fileDir)[0]
-	# get the file name from the full name (e.g. audio)
-	# fileName = os.path.basename(noExten)
 
-	# AudioSegment is from pydub
-	# import in mp3 format
-	audio = AudioSegment.from_mp3(fileDir)
+	# test for existence of matching ogg file
+	if fileDir == '':
+		cont = False
+	elif os.path.exists(noExten + ".ogg"):
+		#ask if you want to replace old file
+		MsgBox = messagebox.askquestion ('Replace existing file','An ogg file already exists. Do you want to replace it?',
+			icon = 'warning')
+		if MsgBox != 'yes':
+			cont = False
 
-	# export in ogg format
-	audio.export(noExten + ".ogg", format="ogg")
+	# only runs if we're good to create/replace ogg file
+	if cont:
+		# import in mp3 format
+		audio = AudioSegment.from_mp3(fileDir)
+
+		# export in ogg format
+		audio.export(noExten + ".ogg", format="ogg")
 
 
 # Creating a Lable Widget
-# myLabel = Label(root, text="Audio Converter")
+# 
 
 # Shove widget onto screen
-# myLabel.grid(row=0, column=0)
+# 
 
-# Creating a button
-myButton = Button(root, text="mp3 to ogg", command=mp3_to_ogg)
-myButton.pack()
+# Creating a button for mp3 to ogg
+Btn1 = Button(root, text="mp3 to ogg", command=mp3_to_ogg, width=20, height=2)
+Btn2 = Button(root, text="ogg to mp3", width=20, height=2)
+Btn1.grid(row=0, column=0)
+Btn2.grid(row=1, column=0)
+
+# Creating a space for text
+textbox = Label(root, text="Audio Converter", width=50, height=5, fg="white", bg="#252525")
+textbox.grid(row=0, column=1, rowspan=2)
 
 # Loop for running program
 root.mainloop()
